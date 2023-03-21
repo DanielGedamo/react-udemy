8688d26205e35f8c57cd9d0f2e5412b71c5f4993
DANIELGEDAMO <danielgadamo@gmail.com>
2023-03-21 18:46:07 +0200

    m

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const connectDB = require('./DB/db')
+
 const app = express()
+
+
+connectDB();
 
 app.get('/',(req,res) => res.send("API Runninig"));
 
+app
 const PORT =process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
