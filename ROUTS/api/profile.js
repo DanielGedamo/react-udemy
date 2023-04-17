e6a695698bf71121e32b6ccd056cf79630462482
DANIELGEDAMO <danielgadamo@gmail.com>
2023-04-17 13:45:50 +0300

    s

--- ROUTS/api/profile.js
+++ ROUTS/api/profile.js
@@ -1,6 +1,23 @@
 const express = require ('express');
 const router = express.Router();
-
-router.get('/',(req,res) => res.send("profile route"));
+const aute = require('../../middelware/auth')
+const Profile = require('../../MODELS/Profile')
+const User = require('../../MODELS/User')
+// get api/profile/me
+// get current user profile
+// access Private
+router.get('/me',aute,async(req,res) =>{
+    try{
+        const profile = await Profile.findOne({user:req.user.id}).populate('user',['name','avatar'])
+        if(!profile){
+            return res.status(500).json({mag:'ther is no profile for this user'})
+        }
+        res.json(profile)
+    }
+    catch(err){
+        console.error(err.message);
+        res.status(500).send('server error')
+    }
+})
 
 module.exports = router;
